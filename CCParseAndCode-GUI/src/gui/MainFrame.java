9d2c08717dda24700584558af9e186cdc7b23790
Tobias Ebbers <tobias@ebbers.biz>
2017-04-25 18:07:07 +0200

    Lock for Single Instance

--- CCParseAndCode-GUI/src/gui/MainFrame.java
+++ CCParseAndCode-GUI/src/gui/MainFrame.java
@@ -16,34 +16,39 @@ public class MainFrame extends Frame {
 	 */
 	private static final long serialVersionUID = 4714864527745266449L;
 
-	public MainFrame(){
+	public MainFrame() {
 		Frame frame = new Frame("CC Log Parse - Auto Code 0.1a");
-		
+
 		Image icon = Toolkit.getDefaultToolkit().getImage(getClass().getClassLoader().getResource("./cc-1.png"));
 		frame.setIconImage(icon);
 		frame.setSize(860, 420);
 		frame.addWindowListener(new WindowAdapter() {
 			// Fensterstatus -> WIRD geschlossen
-			public void windowClosing(WindowEvent ev){
+			public void windowClosing(WindowEvent ev) {
 				frame.dispose();
 			}
-			// Fensterstatus -> IST geschlossen, da einziges fenster automatisch awt exit
-			public void windowClosed(WindowEvent ev){
+
+			// Fensterstatus -> IST geschlossen, da einziges fenster automatisch
+			// awt exit
+			public void windowClosed(WindowEvent ev) {
 				System.exit(0);
 			}
 		});
-		
+
 		new GuiElements(frame);
-		
+
 		frame.setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
 		String lockString = "lockThisAppRightNow";
-		CrossLock lockDouble = new CrossLock(lockString);
-		System.out.print(lockDouble);
-		new MainFrame();
-		
+		CrossLock lockInstance = new CrossLock(lockString);
+		if (lockInstance.lock()) {
+			new MainFrame();
+		} else {
+			System.out.println("Only Single Instance allowed!");
+		}
+
 	}
 
 }
